4756057d3319e0234c99c91067ffa2b639e9779c
Irina <131948761+IrinaMungalov@users.noreply.github.com>
2023-10-16 11:47:33 +0300

    Add files via upload

---
+++ JS for/for-counter.js
@@ -0,0 +1,20 @@
+
+// STANDART FOR
+for (let n=9; n<=9 && n>=1; n--) {
+    console.log(n)
+}
+
+
+//   let n=1
+//   for (; n<=9;) {
+//       console.log(n)
+//      n++
+//   }
+
+
+
+//    let n=1
+//    while (n<=9) {
+//        console.log(n)
+//        n++
+//    }
