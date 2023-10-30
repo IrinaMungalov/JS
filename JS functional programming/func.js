954b66392452755e346b1598e25684e55e7013ea
Irina <131948761+IrinaMungalov@users.noreply.github.com>
2023-10-30 14:52:05 +0200

    Add files via upload

---
+++ JS functional programming/func.js
@@ -0,0 +1,17 @@
+// 3 number ---> in ---> func() ------> out ---->
+
+function sumOfThree (v1,v2,v3) {
+    return v1 + v2 + v3
+}
+
+const sumOfThree = function (v1,v2,v3) {
+    return v1 + v2 + v3
+}
+
+const sumOfThree = (v1,v2,v3) => v1 + v2 + v3
+
+
+
+
+////////////////
+let result = sumOfThree(10,20,30)
