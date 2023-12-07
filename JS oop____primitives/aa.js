ac288d881aadf50f9812915239ace023af6cd1f5
Irina <131948761+IrinaMungalov@users.noreply.github.com>
2023-12-07 21:21:31 +0200

    Add files via upload

---
+++ JS oop____primitives/aa.js
@@ -0,0 +1,27 @@
+let text = "This is a string value example"
+
+console.log(text)
+// < This is a string value example
+
+text.toUpperCase()
+// < 'This is a string value example'
+
+text.newProperty = 'this is a test property'
+// < 'this is a test property'
+
+text.newProperty
+// < undefined
+
+
+
+
+
+
+
+
+
+
+
+
+
+
